0abc419b2cbb4748c1e93876d8c3d77bbc2cc356
Amiin Farahbakhsh <farahbakhsh3@gmail.com>
2023-11-30 13:12:17 +0330

    Add Scanner and a bit of optimization. 😁

--- src/fibo/Fibo.java
+++ src/fibo/Fibo.java
@@ -1,18 +1,27 @@
 package fibo;
 
+import java.util.Scanner;
+
 public class Fibo {
 
-    static int fibonachi(int n){
-        if ((n==0) || (n==1))
-                return 1;
-        return fibonachi(n-1)+fibonachi(n-2);
+    static long fibonachi(int n) {
+        if ((n == 0) || (n == 1)) {
+            return 1;
+        }
+
+        return fibonachi(n - 1) + fibonachi(n - 2);
     }
+
     public static void main(String[] args) {
-            int n = 45;
-            System.out.println(String.format("fibo(%d) = %d", n, fibonachi(n)));
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input n: ");
+        int n = input.nextInt();
+
+        if (n < 0) {
+            System.err.print("Wrong n.");
+            return;
+        }
+        System.out.format("fibonachi(%d) = %d\n", n, fibonachi(n));
     }
 }
-
-
-
-
