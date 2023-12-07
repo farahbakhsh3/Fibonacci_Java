419eee8cd9c8d8b1c5ccda92d6857185ab434023
Amiin Farahbakhsh <farahbakhsh3@gmail.com>
2023-12-07 12:04:45 +0330

    a bit of code cleaning. 😊

--- src/fibo/Fibonachi.java
+++ src/fibo/Fibonachi.java
@@ -1,23 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package fibo;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *
- * @author 2630856267
- */
 class Fibonachi {
 
     private Map<Long, Long> previousValuesHolder;
 
     public Fibonachi() {
         this.previousValuesHolder = new HashMap<Long, Long>();
-        this.previousValuesHolder.put(Long.valueOf(0), Long.valueOf(1));
+        this.previousValuesHolder.put(Long.valueOf(0), Long.valueOf(0));
         this.previousValuesHolder.put(Long.valueOf(1), Long.valueOf(1));
     }
 
@@ -26,14 +19,14 @@ class Fibonachi {
             return this.previousValuesHolder.get(n);
         }
 
-        long newValue = this.fibonachi_optimized(n - 2) + this.fibonachi_optimized(n - 1);
+        long newValue = this.fibonachi_optimized(n - 1) + this.fibonachi_optimized(n - 2);
         this.previousValuesHolder.put(Long.valueOf(n), Long.valueOf(newValue));
         return newValue;
     }
 
     public long fibonachi_standard(int n) {
         if ((n == 0) || (n == 1)) {
-            return 1;
+            return n;
         }
 
         return this.fibonachi_standard(n - 1) + this.fibonachi_standard(n - 2);
