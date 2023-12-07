42b45722a27759e4b62c22465ca7eb35cddacf0f
Amiin Farahbakhsh <farahbakhsh3@gmail.com>
2023-12-07 12:07:07 +0330

    a bit of code cleaning. v2 😊

--- src/fibo/Fibo.java
+++ src/fibo/Fibo.java
@@ -1,23 +1,21 @@
 package fibo;
 
-import java.util.Scanner;
+public class Fibo {
 
-public class Fibo {    
     public static void main(String[] args) {
 
-        Fibonachi fibonachi = new Fibonachi();
+        Fibonacci fibonacci = new Fibonacci();
 
-        int n = 50;
+        int n = 10;
 
         long beginTime = System.currentTimeMillis();
-        long fibo = fibonachi.fibonachi_standard(n);
+        long fibo = fibonacci.fibonacci_standard(n);
         long endTime = System.currentTimeMillis();
         long delta = endTime - beginTime;
         System.out.println("Standard mode :: F(" + n + ") = " + fibo + " ... computed     in " + delta + " milliseconds");
-        
-        
+
         beginTime = System.currentTimeMillis();
-        fibo = fibonachi.fibonachi_optimized(n);
+        fibo = fibonacci.fibonacci_optimized(n);
         endTime = System.currentTimeMillis();
         delta = endTime - beginTime;
         System.out.println("Optimized mode :: F(" + n + ") = " + fibo + " ... computed     in " + delta + " milliseconds");
--- src/fibo/Fibonacci.java
+++ src/fibo/Fibonacci.java
@@ -4,31 +4,31 @@ package fibo;
 import java.util.HashMap;
 import java.util.Map;
 
-class Fibonachi {
+class Fibonacci {
 
     private Map<Long, Long> previousValuesHolder;
 
-    public Fibonachi() {
+    public Fibonacci() {
         this.previousValuesHolder = new HashMap<Long, Long>();
         this.previousValuesHolder.put(Long.valueOf(0), Long.valueOf(0));
         this.previousValuesHolder.put(Long.valueOf(1), Long.valueOf(1));
     }
 
-    public long fibonachi_optimized(long n) {
+    public long fibonacci_optimized(long n) {
         if (this.previousValuesHolder.containsKey(Long.valueOf(n))) {
             return this.previousValuesHolder.get(n);
         }
 
-        long newValue = this.fibonachi_optimized(n - 1) + this.fibonachi_optimized(n - 2);
+        long newValue = this.fibonacci_optimized(n - 1) + this.fibonacci_optimized(n - 2);
         this.previousValuesHolder.put(Long.valueOf(n), Long.valueOf(newValue));
         return newValue;
     }
 
-    public long fibonachi_standard(int n) {
+    public long fibonacci_standard(int n) {
         if ((n == 0) || (n == 1)) {
             return n;
         }
 
-        return this.fibonachi_standard(n - 1) + this.fibonachi_standard(n - 2);
+        return this.fibonacci_standard(n - 1) + this.fibonacci_standard(n - 2);
     }
 }
